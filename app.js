1291c3cbd0f63ebb277a4a66e985c39d5a20dfd9
Martinsanchez06 <sanchezmartin2435gmail.com>
2022-12-02 08:11:33 -0500

    Header desktop hecho y una parte más

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,11 +19,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+// ----- PORT ----- 
+const port = process.env.PORT || 4010;
+app.listen(port, () =>{
+
+    console.log(`listening on port ${port}`);
 });
 
 // error handler
@@ -35,7 +40,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
